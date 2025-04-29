1f44b2e74cc159b7528c025173f5779a30bae31f
samitkapoor <samitkapoor77@gmail.com>
2025-04-29 17:54:56 +0530

    Add footer with attribution to Samit Kapoor in layout.tsx for enhanced branding and user engagement.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.className} antialiased overflow-x-hidden overflow-y-auto`}>
+      <body
+        className={`${geistSans.className} antialiased overflow-x-hidden overflow-y-auto relative`}
+      >
         {children}
+        <p className="absolute bottom-10 text-center w-full">
+          Made with ❤️ by{' '}
+          <a
+            href="https://samitkapoor.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline font-medium text-orange-500"
+          >
+            Samit Kapoor
+          </a>
+        </p>
       </body>
     </html>
   );
