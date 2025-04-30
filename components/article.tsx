d8a8e453eb992064906af9eea5afa7c13c744ccc
samitkapoor <samitkapoor77@gmail.com>
2025-04-30 15:18:15 +0530

    Update Article component styles for improved responsiveness and visual consistency.

--- components/article.tsx
+++ components/article.tsx
@@ -10,7 +10,7 @@ const Article = () => {
     <Link
       href="https://dev.to/samitkapoor/how-to-make-your-website-link-look-amazing-on-social-media-58n1"
       target="_blank"
-      className="mt-10 shadow-xl group hover:scale-105 transition-all duration-300"
+      className="mt-10 shadow-xl group hover:scale-105 transition-all duration-300 mx-4"
     >
       <div className="flex flex-col rounded-lg overflow-hidden bg-black">
         <div className="max-w-xl w-full max-h-[240px] h-full overflow-hidden relative">
@@ -23,10 +23,10 @@ const Article = () => {
           />
           <div className="absolute bottom-0 left-0 right-0 w-full bg-black h-[80px] blur-lg z-10"></div>
           <div className="absolute bottom-0 left-0 right-0 w-full bg-black h-[80px] z-10 flex items-end justify-end flex-col p-4">
-            <p className="text-white text-center text-base w-full font-bold z-20">
+            <p className="text-xs sm:text-sm md:text-base text-white text-center w-full font-bold z-20">
               How to Make Your Website Link Look Amazing on Social Media?
             </p>
-            <p className="w-full text-orange-500 text-center text-sm font-bold z-20 group-hover:translate-x-1 transition-all duration-300">
+            <p className="text-xs sm:text-sm md:text-base w-full text-orange-500 text-center font-bold z-20 group-hover:translate-x-1 transition-all duration-300">
               Read {'->'}
             </p>
           </div>
