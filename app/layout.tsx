c7272312958b61e494f91dfd18a544c8c78087f6
samitkapoor <samitkapoor77@gmail.com>
2025-04-30 11:55:13 +0530

    added open graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,32 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
   title: 'peeklink',
-  description: 'Perfect Your Presence Before You Share.'
+  description: 'Perfect Your Presence Before You Share.',
+  openGraph: {
+    title: 'peeklink - Perfect Your Presence Before You Share.',
+    description:
+      'peeklink shows you how your links will look like on social media before you share them. It helps you perfect your presence before you share.',
+    url: 'https://peeklink.stackbits.dev',
+    siteName: 'peeklink',
+    images: [
+      {
+        url: 'https://peeklink.stackbits.dev/peeklink-poster.png',
+        width: 1200,
+        height: 630,
+        alt: 'peeklink - Perfect Your Presence Before You Share.'
+      }
+    ],
+    locale: 'en_US',
+    type: 'website'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'peeklink - Perfect Your Presence Before You Share.',
+    description:
+      'peeklink shows you how your links will look like on social media before you share them. It helps you perfect your presence before you share.',
+    creator: '@samitkapoorr',
+    images: ['https://peeklink.stackbits.dev/peeklink-poster.png']
+  }
 };
 
 export default function RootLayout({
